3bd049bc88bd2b57574dbf5b7a1424b4715a53ae
sadegh shokhan <sadegh_shokhan@yahoo.com>
2026-09-20 11:55:58 +0330

    wrote some logic of TimerChallenge.jsx for stop & start perpose

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,15 +1,35 @@
+import { useRef, useState } from "react"
+
 export default function TimerChallenge({title , targetTime}){
+    const [timerExpired , setTimerExpired] = useState(false);
+    const [timerStarted , setTimerStarted] = useState(false);
+    const timer = useRef()
+
+
+    function handleStartClick(){
+    timer.current = setTimeout(() => {
+        setTimerExpired(true);
+    }, targetTime * 1000 );
+
+    setTimerStarted(true);
+    }
+
+    function handleStopClick(){
+        clearTimeout(timer.current)
+    }
+
     return (
         <section className= "challenge">
             <h2>{title}</h2>
+            <p>{timerExpired && "you lost!"}</p>
             <p className="challenge-time">
                 {targetTime} second{targetTime > 1 ? "'s" : ""}
             </p>
-            <button>
-                start challenge
+            <button onClick={timerStarted ? handleStopClick : handleStartClick}>
+                {timerStarted ? "stop challenge" : "start challenge"}
             </button>
-            <p className="">
-                time is running...
+            <p className={timerStarted ? "active" : undefined}>
+                {timerStarted ? "time is running..."  : "just click start"}
             </p>
         </section>
     )
