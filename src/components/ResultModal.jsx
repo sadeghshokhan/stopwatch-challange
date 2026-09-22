a842f086991d305dbe28b6ed78b57064eae412a8
sadegh shokhan <sadegh_shokhan@yahoo.com>
2026-09-22 10:25:41 +0330

    score add to challenge

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,11 +1,11 @@
 import { useImperativeHandle, useRef } from "react"
 
-export default function ResultModal({ targetTime , ref , remainingTime , onRestart}){
+export default function ResultModal({ targetTime, ref, remainingTime, onRestart }) {
     const dialog = useRef()
 
-    useImperativeHandle(ref , ()=>{
+    useImperativeHandle(ref, () => {
         return {
-            open(){
+            open() {
                 dialog.current.showModal()
             }
         }
@@ -13,10 +13,11 @@ export default function ResultModal({ targetTime , ref , remainingTime , onResta
 
     const lostTime = remainingTime <= 0
     const calculateTimeRamaining = (remainingTime / 1000).toFixed(2);
-
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
     return (
         <dialog className="result-modal" ref={dialog}>
-            <h2>{lostTime && <p>Ridi</p>}</h2>
+            {lostTime && <h2>Ridi</h2>}
+            {!lostTime && <h2>your score: {score}</h2>}
             <p>the target time was <strong>{targetTime} seconds.</strong></p>
             <p>you stop the timer with <strong>{calculateTimeRamaining} seconds left.</strong></p>
             <form method="dialog" onSubmit={onRestart}>
